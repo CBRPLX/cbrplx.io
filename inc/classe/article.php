1ba1c2ac1fe4811aad9b9bcabbc4f0bfe4430e3d
Cyberplix <robin.pierrot@gmail.com>
2015-02-10 16:02:54 +0100

    Deleting images

--- inc/classe/article.php
+++ inc/classe/article.php
@@ -119,6 +119,20 @@ class article{
         $stmt->execute(array($this->id_article));
 
         if($stmt->rowCount() > 0){
+            $dir = "assets/".$this->id_article;
+            if(is_dir($dir)){
+                $it = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
+                $files = new \RecursiveIteratorIterator($it,
+                             \RecursiveIteratorIterator::CHILD_FIRST);
+                foreach($files as $file) {
+                    if ($file->isDir()){
+                        rmdir($file->getRealPath());
+                    } else {
+                        unlink($file->getRealPath());
+                    }
+                }
+                rmdir($dir);
+            }
             return true;
         }else{
             return false;
