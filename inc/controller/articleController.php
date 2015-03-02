a6db37aea9746e321a48ce5654bab49034cdea83
Cyberplix <robin.pierrot@gmail.com>
2015-03-02 17:00:22 +0100

    Link to profile

--- inc/controller/articleController.php
+++ inc/controller/articleController.php
@@ -27,30 +27,33 @@ class articleController {
             $all_technos = new \classe\techno();
             $all_technos = $all_technos->load($article->get('id_article'));
             $tri_technos = array();
-            $technos = array();
-            $i = 0;
-            foreach ($all_technos as $k => $v) {
-                if($i < 3){ //Si on a pas besoin de créer une nouvelle ligne
-                    array_push($technos, $v);
-                    $i++;
-                }else{ //Si on doit créer une nouvelle ligne
-                    array_push($tri_technos, $technos);
-                    $technos = array();
-                    array_push($technos, $v);
-                    $i = 1;
-                }
-            }
-            if($i == 3){
-                array_push($tri_technos, $technos);
+
+            if($all_technos !== false){
                 $technos = array();
                 $i = 0;
-            }
+                foreach ($all_technos as $k => $v) {
+                    if($i < 3){ //Si on a pas besoin de créer une nouvelle ligne
+                        array_push($technos, $v);
+                        $i++;
+                    }else{ //Si on doit créer une nouvelle ligne
+                        array_push($tri_technos, $technos);
+                        $technos = array();
+                        array_push($technos, $v);
+                        $i = 1;
+                    }
+                }
+                if($i == 3){
+                    array_push($tri_technos, $technos);
+                    $technos = array();
+                    $i = 0;
+                }
 
-            while ($i < 3) {
-                array_push($technos, "");
-                $i++;
+                while ($i < 3) {
+                    array_push($technos, "");
+                    $i++;
+                }
+                array_push($tri_technos, $technos);
             }
-            array_push($tri_technos, $technos);
 
             $contenu = $template->render(array(
                 'article' => $article,
