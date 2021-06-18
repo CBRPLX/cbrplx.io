5887137a3996d29de23cb69ffa6963f734c519ac
Robin Pierrot <robin@neo-nomade.com>
2021-06-18 17:26:55 +0200

    Maj Twig and PHP 7.4

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
     '253c157292f75eb38082b5acb06f3f01' => $vendorDir . '/nikic/fast-route/src/functions.php',
 );
